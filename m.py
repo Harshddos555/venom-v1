1d45f3e1d9dee0af3b5654adb9134ad80c486e8b
Harshddos555 <harshbambhaniya2512@gmail.com>
2024-09-09 15:16:58 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,16 +8,16 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7294799557:AAHe5OMcEhYnbRYgrb-iDRLmgWbhLTHJGGg')
 
 # Admin user IDs
-admin_id = ["5894556386"]
+admin_id = ["6443199240"]
 
 # File to store allowed user IDs
-USER_FILE = "users.txt"
+USER_FILE = "Harsh_ddos_bot"
 
 # File to store command logs
-LOG_FILE = "log.txt"
+LOG_FILE = "Harsh_ddos_bot"
 
 # Function to read user IDs from the file
 def read_users():
